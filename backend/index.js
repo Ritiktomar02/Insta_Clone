13e788977d0395a69e7ad6e0391a4cceeeb07531
Ritik Tomar <ritiktomar@Ritiks-MacBook-Air.local>
2025-02-11 16:03:24 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,45 @@
-const express=require('express');
-const cors=require('cors');
-const cookieParser=require('cookie-parser');
-const fileUpload=require('express-fileupload');
-require('dotenv').config();
-const app=express();
+const express = require("express");
+const { urlencoded } = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const connectDB = require("./utils/db.js");
+const userRoute = require("./routes/user.route.js");
+const postRoute = require("./routes/post.route.js");
+const messageRoute = require("./routes/message.route.js");
+const {app, server} = require("./socket/socket.js");
 
-const PORT=process.env.PORT || 4001;
+ 
 
-require("./config/database").connect()
 
-app.use(cors({
-    origin:['http://localhost:5173'],
-    credentials:true
-}));
+const PORT = process.env.PORT || 3000;
 
+
+
+//middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
 
-app.use(fileUpload({
-    useTempFiles:true,
-    tempFileDir:'/tmp/'
-}))
+
+const corsOptions = {
+    origin: process.env.URL,
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+// yha pr apni api ayengi
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
+
 
 app.get("/", (req, res) => {
-	return res.json({
-		success: true,
-		message: "Your server is up and running ...",
-	});
-});
-
-app.listen(PORT, () => {
-	console.log(`App is listening at ${PORT}`);
+    res.send("Hello World");
 });
 
 
+server.listen(PORT, () => {
+    connectDB();
+    console.log(`Server listen at port ${PORT}`);
+});
